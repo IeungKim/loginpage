d591ef5237c6b6431baa6f65fb243e8dbfcf9ec2
IeungKim <100753588+IeungKim@users.noreply.github.com>
2022-03-17 16:31:06 +0900

    Update LDH_LION.js

--- LDH_LION.js
+++ LDH_LION.js
@@ -2,7 +2,7 @@ var checkbox = document.querySelector("#checkbox");
 checkbox.onclick = changePic;
 
 function changePic() {
-  if (checkbox.src.match('_empty')) { //안 체크된 거일때
+  if (checkbox.src.match('_empty')) { //체크 안된 이미지일 때
     checkbox.src = 'LDH_LION_img/icon_check.png';
   }  
   else { //체크된 이미지일 때
